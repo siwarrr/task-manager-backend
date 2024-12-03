439711df3f62cad4c561e536859519baf5d90c1e
siwarrr <76534931+siwarrr@users.noreply.github.com>
2024-12-03 15:31:24 +0100

    Configured API URLs for deployment, updated CORS settings

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:3000"
+        origin: ["https://task-manager-front-neon.vercel.app/"]
     })
 );
 app.use(express.json());
